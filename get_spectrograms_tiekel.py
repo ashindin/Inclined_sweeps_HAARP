fe932cbba3bdd3b556b45875cca5ce336533d3a1
ashindin <shindin@rf.unn.ru>
2019-03-21 20:43:10 +0300

    add file

--- get_spectrograms_tiekel.py
+++ get_spectrograms_tiekel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
+# In[1]:
 
 
 import os, sys
@@ -10,7 +10,7 @@ import scipy.signal as ss
 import matplotlib.pyplot as plt
 
 
-# In[20]:
+# In[11]:
 
 
 def progress(count, total, status=''):
@@ -22,7 +22,7 @@ def progress(count, total, status=''):
     sys.stdout.flush() # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the
 def get_raw_data_from_binary_file(fname,offset_samples,duration_samples,bit_depth,num_of_channels):
     f=open(fname,'rb')
-    offset_bytes=offset_samples*(bit_depth//8)*num_of_channels
+    offset_bytes=int(offset_samples*int(bit_depth/8)*num_of_channels)
 #     print(offset_bytes)
     f.seek(offset_bytes,0)
     data_raw=f.read(int(duration_samples*int(bit_depth/8)*num_of_channels))
@@ -30,8 +30,7 @@ def get_raw_data_from_binary_file(fname,offset_samples,duration_samples,bit_dept
     return data_raw
 def raw_to_complex_volts(data_raw,nc,v_range,bit_depth=16):
     if bit_depth==16:
-#         data=np.fromstring(data_raw,dtype=np.int16)
-        data=np.frombuffer(data_raw,dtype=np.int16)
+        data=np.frombuffer(data_raw,dtype='>i2')
         data=np.reshape(data,(nc,int(len(data_raw)/nc/2)),'F')*v_range/32767
         data_complex=np.zeros((int(nc/2),int(len(data_raw)/nc/2)),dtype=np.complex128)
         for i in range(int(nc/2)):
@@ -43,34 +42,40 @@ def raw_to_complex_volts(data_raw,nc,v_range,bit_depth=16):
         for i in range(int(nc/2)):
             data_complex[i,:]=data[2*i,:]+1j*data[2*i+1,:]
     return data_complex
+def raw_to_int16(data_raw):
+    data=np.frombuffer(data_raw,dtype='>i2')
+    return data
 def get_spectrogram(fname, offset_samples, period_samples, duration_samples, sr,bd,nc, channel=0, nfft=0, num_of_pulses=0,v_range=0.5,waitbar=True):    
 #     sr,bd,nc,ns=get_wv_file_parameters(fname)
     fsize=os.path.getsize(fname) # file size
-    ns=fsize//(bd//8)//nc
-    
+    ns=int(fsize/int(bd/8)/nc)
     if nfft==0:
         nfft=int(sr/1000) # freq. res. 1kHz
     if num_of_pulses==0:
         num_of_pulses=int((ns-offset_samples)/period_samples)
-    t_axe=np.array([i*period_samples/sr for i in range(num_of_pulses)])
+#     t_axe=np.array([i*period_samples/sr for i in range(num_of_pulses)])
     SPECTROGRAM=np.zeros((num_of_pulses,nfft))
     if waitbar==True:
         for i in range(num_of_pulses):
-#             data_raw=get_raw_data_from_wv_file(fname,offset_samples+i*period_samples,duration_samples)
             progress(i, num_of_pulses, status='Calculating spectrogram')
+#             data_raw=get_raw_data_from_wv_file(fname,offset_samples+i*period_samples,duration_samples)
             data_raw=get_raw_data_from_binary_file(fname,offset_samples+i*period_samples,duration_samples,bd,nc)
             data_complex=raw_to_complex_volts(data_raw,nc,v_range,bit_depth=bd)[channel,:]
-            f_axe,spec=ss.welch(data_complex,fs=sr,nperseg=nfft,noverlap = nfft-1,scaling='spectrum',window='hanning',detrend=None, return_onesided=False)
+            f_axe,spec=ss.welch(data_complex,fs=sr,nperseg=nfft,noverlap = nfft-1,scaling='spectrum',window='hanning',detrend=None,return_onesided=False)
             SPECTROGRAM[i,:]=np.hstack((spec[int(nfft/2)::],spec[0:int(nfft/2)]))    
     else:
         for i in range(num_of_pulses):
 #             data_raw=get_raw_data_from_wv_file(fname,offset_samples+i*period_samples,duration_samples)
             data_raw=get_raw_data_from_binary_file(fname,offset_samples+i*period_samples,duration_samples,bd,nc)
             data_complex=raw_to_complex_volts(data_raw,nc,v_range,bit_depth=bd)[channel,:]
-            f_axe,spec=ss.welch(data_complex,fs=sr,nperseg=nfft,noverlap = nfft-1,scaling='spectrum',window='hanning',detrend=None, return_onesided=False)
+            f_axe,spec=ss.welch(data_complex,fs=sr,nperseg=nfft,noverlap = nfft-1,scaling='spectrum',window='hanning',detrend=None,return_onesided=False)
             SPECTROGRAM[i,:]=np.hstack((spec[int(nfft/2)::],spec[0:int(nfft/2)])) 
     f_axe=np.hstack((f_axe[int(nfft/2)::],f_axe[0:int(nfft/2)]))
-    return np.fliplr(SPECTROGRAM), f_axe, t_axe # ВНИМАНИЕ! Для данных Сергеева берем fliplr(SPECTROGRAM) вместо SPECTROGRAM
+    if nfft % 2 != 0:
+        f_axe=np.roll(f_axe,-1)
+        spec=np.roll(f_axe,-1)
+    t_axe=np.arange(0,SPECTROGRAM.shape[0]*0.4,0.4)
+    return SPECTROGRAM, f_axe, t_axe
 def view_spectrogram_dbshift(t_axe,f_axe,spectrogram,tit='',min_int=-120,max_int=-90,ymin=0,ymax=0,db_shift=0,savefile="",close_fig=False):
     fig=plt.figure(figsize=(9,6))
     plt.pcolormesh(t_axe,f_axe,10*np.log10(spectrogram.T)+db_shift,vmax=max_int,vmin=min_int,cmap='jet')
@@ -80,7 +85,7 @@ def view_spectrogram_dbshift(t_axe,f_axe,spectrogram,tit='',min_int=-120,max_int
         ymin=f_axe[0]
         ymax=f_axe[-1]
     plt.ylim(ymin,ymax)
-    plt.grid()
+#     plt.grid()
     plt.colorbar()
     plt.title(tit)    
     if savefile!="":
@@ -90,47 +95,60 @@ def view_spectrogram_dbshift(t_axe,f_axe,spectrogram,tit='',min_int=-120,max_int
         plt.show()
 
 
-# In[3]:
+# In[7]:
 
 
-bin_fnames=['2903201114.raw','2903201115.raw']
+fname="rxdsp-haarp-20110330-012352-p01.data"
+print(fname)
+bd=16; nc=2; sr=333333; v_range=0.5;
+nfft=333
 
 
-# In[8]:
+# In[9]:
 
 
-sr=500000;bd=16;nc=2;v_range=0.5;
-nfft=500
-period_samples=int(0.2*sr)
-duration_samples=int(0.15*sr)
-f_center=np.ones(len(bin_fnames),dtype=np.int)*5780000;
-start_offsets=np.array([55000, 56000])
-
-
-# In[15]:
-
-
-npz_fnames=[os.path.split(bin_fnames[i])[1].split('.')[0]+"_spm_n"+str(nfft)+".npz" for i in range(len(bin_fnames))]
+npz_fnames=fname.split('.')[0]+"_spm_n"+str(nfft)+".npz"
 # print(npz_fnames)
-png_fnames=[os.path.split(bin_fnames[i])[1].split('.')[0]+"_spm_n"+str(nfft)+".png" for i in range(len(bin_fnames))]
+png_fnames=fname.split('.')[0]+"_spm_n"+str(nfft)+".png"
 # print(png_fnames)
 
 
 # In[ ]:
 
 
-for i in range(len(bin_fnames)):
-    save_filename=npz_fnames[i]
-    print("Processing "+bin_fnames[i])
-    SP_RAW, f_axe, t_axe=get_spectrogram(bin_fnames[i], start_offsets[i], period_samples, duration_samples, sr,bd,nc, channel=0, nfft=nfft, num_of_pulses=0,v_range=0.5,waitbar=True)        
-    np.savez(save_filename,
-         source_filename=os.path.split(bin_fnames[i])[1],
-         spectrogram=SP_RAW,
-         f_axe=f_axe, t_axe=t_axe, nfft=nfft,
-         offset_samples=start_offsets[i],
-         duration_samples=duration_samples,
-         period_samples=period_samples)
-    view_spectrogram_dbshift(t_axe,f_axe,SP_RAW,tit=os.path.split(bin_fnames[i])[1],
-                             min_int=-120,max_int=-80,ymin=-70,ymax=230,
-                             db_shift=0,savefile=png_fnames[i])
+offset_samples=23215; period_samples=32772; duration_samples=11000
+SP1, f_axe1, t_axe1=get_spectrogram(fname, offset_samples, period_samples, duration_samples, sr,bd,nc, channel=0, nfft=nfft, num_of_pulses=0,v_range=0.5,waitbar=True)
+offset_samples=8215; period_samples=32772; duration_samples=11000
+SP2, f_axe2, t_axe2=get_spectrogram(fname, offset_samples, period_samples, duration_samples, sr,bd,nc, channel=0, nfft=nfft, num_of_pulses=0,v_range=0.5,waitbar=True)   
+
+
+# In[ ]:
+
+
+SP1=SP1[919:-1,:]
+SP2=SP2[920::,:]
+SPCAT=np.zeros((SP1.shape[0]+SP2.shape[0],SP1.shape[1]))
+for i in range(SP1.shape[0]):
+    SPCAT[2*i,:]=SP1[i,:]
+    SPCAT[2*i+1,:]=SP2[i,:]
+SPCAT=SPCAT[0:13500,:]
+t_axe=np.array([i*0.4 for i in range(SPCAT.shape[0])])
+
+
+# In[ ]:
+
+
+np.savez(npz_fnames,
+     spectrogram=SPCAT,
+     f_axe=f_axe1, t_axe=t_axe, nfft=nfft,     
+     duration_samples=duration_samples,
+     period_samples=period_samples)
+
+
+# In[ ]:
+
+
+view_spectrogram_dbshift(t_axe,f_axe1/1000,SPCAT,tit=fname,
+                             min_int=-120,max_int=-80,ymin=-125,ymax=165,
+                             db_shift=0,savefile=png_fnames)
 
