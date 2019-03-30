1cdc21c582b0f1031426c597e2ed1b23aae316a7
ashindin <shindin@rf.unn.ru>
2019-03-30 17:25:47 +0300

    fix file

--- fill_database.py
+++ fill_database.py
@@ -37,11 +37,11 @@ t_axe_local=np.arange(0.,40.,.2)
 
 
 inline_spm_fnames=[
-'f0shifted_spm_A_n250.npz',
-'f0shifted_spm_B_n500.npz',
-'f0shifted_spm_C_n333.npz'
+'f0shifted_spm_A_n2500.npz',
+'f0shifted_spm_B_n5000.npz',
+'f0shifted_spm_C_n3330.npz'
 ]
-output_fname='spm_database.npy'
+output_fname='spm_database_100Hz.npy'
 
 
 # In[7]:
@@ -71,47 +71,6 @@ for site_ind in range(3):
 
 np.save(output_fname,SPM_DB)
 
-
-# In[9]:
-
-
-inline_spm_fnames=[
-'f0shifted_spm_A_n250_filt.npz',
-'f0shifted_spm_B_n500.npz',
-'f0shifted_spm_C_n333.npz'
-]
-output_fname='spm_database_filt.npy'
-
-
-# In[10]:
-
-
-SPM_DB=[]
-for site_ind in range(3):
-    npz_data=np.load(inline_spm_fnames[site_ind]);
-    spectrogram=npz_data["spectrogram"];
-    f_axe=npz_data["f_axe"];
-    t_axe=npz_data["t_axe"];
-    nfft=npz_data["nfft"];
-#     f_center=npz_data["f_center"];
-#     site_file=npz_data["site"];
-    
-    SPM_DB.append([])
-    for angle_ind in range(9):
-        SPM_DB[site_ind].append([])
-        for dir_ind in range(2):
-            SPM_DB[site_ind][angle_ind].append([])
-            for series_ind in range(2):
-                st_ind=get_st_t_indxs(series_ind=series_ind,angle_ind=angle_ind,dir_ind=dir_ind)
-                if dir_ind==0:
-                    SPM_DB[site_ind][angle_ind][dir_ind].append((f0_axe_down,f_axe,spectrogram[st_ind:st_ind+200,:]))                
-                else:
-                    SPM_DB[site_ind][angle_ind][dir_ind].append((f0_axe_up,f_axe,spectrogram[st_ind:st_ind+200,:]))        
-
-np.save(output_fname,SPM_DB)
-
-
-# In[ ]:
 
 
 
